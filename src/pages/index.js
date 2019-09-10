aad686105a229ab7b8f448e083e7fc8e2925da57
Siddharth Kundu <sk31395@gmail.com>
2019-09-11 00:10:46 +0530

    add markdown query and staticQuery

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,62 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql, StaticQuery } from "gatsby"
+import Post from "../components/Post"
+import { id } from "postcss-selector-parser"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home Page</h1>
+    <StaticQuery
+      query={indexQuery}
+      render={data => {
+        return (
+          <div>
+            {data.allMarkdownRemark.edges.map(({ node }) => {
+              const {
+                id,
+                frontmatter: { title, date, author, path },
+                excerpt,
+              } = node
+
+              return (
+                <Post
+                  key={id}
+                  title={title}
+                  date={date}
+                  author={author}
+                  path={path}
+                  body={excerpt}
+                  path={path}
+                />
+              )
+            })}
+          </div>
+        )
+      }}
+    />
   </Layout>
 )
+
+const indexQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "Do MMM YYYY")
+            author
+            path
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
