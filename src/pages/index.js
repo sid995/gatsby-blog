434f5657b6cb4e25438ede4db8b56cfbbb063338
Siddharth Kundu <sk31395@gmail.com>
2019-09-13 23:38:34 +0530

    Add query for img

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,40 +4,61 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql, StaticQuery } from "gatsby"
 import Post from "../components/Post"
-import { id } from "postcss-selector-parser"
+import { Row, Col } from "reactstrap"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home Page</h1>
-    <StaticQuery
-      query={indexQuery}
-      render={data => {
-        return (
-          <div>
-            {data.allMarkdownRemark.edges.map(({ node }) => {
-              const {
-                id,
-                frontmatter: { title, date, author, path },
-                excerpt,
-              } = node
+    <Row>
+      <Col md="8">
+        <StaticQuery
+          query={indexQuery}
+          render={data => {
+            return (
+              <div>
+                {data.allMarkdownRemark.edges.map(({ node }) => {
+                  const {
+                    id,
+                    frontmatter: {
+                      title,
+                      date,
+                      author,
+                      path,
+                      image: {
+                        childImageSharp: { fluid },
+                      },
+                    },
+                    excerpt,
+                  } = node
 
-              return (
-                <Post
-                  key={id}
-                  title={title}
-                  date={date}
-                  author={author}
-                  path={path}
-                  body={excerpt}
-                  path={path}
-                />
-              )
-            })}
-          </div>
-        )
-      }}
-    />
+                  return (
+                    <Post
+                      key={id}
+                      title={title}
+                      date={date}
+                      author={author}
+                      path={path}
+                      body={excerpt}
+                      fluid={fluid}
+                    />
+                  )
+                })}
+              </div>
+            )
+          }}
+        />
+      </Col>
+      <Col md="4">
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0,0,0,0.4)",
+          }}
+        ></div>
+      </Col>
+    </Row>
   </Layout>
 )
 
@@ -52,6 +73,13 @@ const indexQuery = graphql`
             date(formatString: "Do MMM YYYY")
             author
             path
+            image {
+              childImageSharp {
+                fluid(maxWidth: 600) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
           }
           excerpt
         }
