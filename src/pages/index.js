ef893d5a6994ce16874384df263449180f728bb4
Siddharth Kundu <sk31395@gmail.com>
2019-09-14 23:24:49 +0530

    added sidebar component

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import SEO from "../components/seo"
 import { graphql, StaticQuery } from "gatsby"
 import Post from "../components/Post"
 import { Row, Col } from "reactstrap"
+import Sidebar from "../components/Sidebar"
 
 const IndexPage = () => (
   <Layout>
@@ -52,13 +53,7 @@ const IndexPage = () => (
         />
       </Col>
       <Col md="4">
-        <div
-          style={{
-            width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(0,0,0,0.4)",
-          }}
-        ></div>
+        <Sidebar />
       </Col>
     </Row>
   </Layout>
